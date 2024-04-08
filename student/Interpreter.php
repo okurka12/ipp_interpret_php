ccb4658cde92a8f87b0b60d0cce0a685c8bcd774
Vit Pavlik <pavlikvitek13@gmail.com>
2024-04-08 23:19:28 +0200

    catch valueerror on empty source xml

--- student/Interpreter.php
+++ student/Interpreter.php
@@ -2,8 +2,30 @@
 
 namespace IPP\Student;
 
+use DOMDocument;
 use IPP\Core\AbstractInterpreter;
 use IPP\Core\Exception\NotImplementedException;
+use IPP\Core\Exception\XMLException;
+use ValueError;
+
+class Instruction
+{
+    public function init (): void
+    {
+
+    }
+}
+
+class InstructionList
+{
+    /** @var array<Instruction> */
+    public array $list;
+
+    public function init(DOMDocument $dom): void
+    {
+        return;
+    }
+}
 
 class Interpreter extends AbstractInterpreter
 {
@@ -11,10 +33,24 @@ class Interpreter extends AbstractInterpreter
     {
         // TODO: Start your code here
         // Check \IPP\Core\AbstractInterpreter for predefined I/O objects:
-        // $dom = $this->source->getDOMDocument();
+
+        try
+        {
+            $dom = $this->source->getDOMDocument();
+        }
+
+        /* this exception is uncaught elsewhere and it happens when the source
+        file is empty */
+        catch (ValueError) {
+            throw new XMLException("empty xml");
+        }
+
         // $val = $this->input->readString();
         // $this->stdout->writeString("stdout");
         // $this->stderr->writeString("stderr");
-        throw new NotImplementedException;
+        $this->stdout->writeString("ahoj svete\n");
+        $k = new InstructionList;
+        return 0;
+        // throw new NotImplementedException;
     }
 }
